b2b00d9bda132dcb7ad946d9d779eefb3420fc74
Rachel Kao <rachelsingkao@gmail.com>
2024-09-16 20:52:47 -0700

    step 1 and 2

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const app = express()
+
+app.use('/public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style = "text-align: center; margin-top: 50px;">Unearthed API</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log('Server listening on http://localhost:${PORT}')
+})
