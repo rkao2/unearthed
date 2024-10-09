251a01862d09340dd0d41ce1ebd11c27de66d6ae
Rachel Kao <rachelsingkao@gmail.com>
2024-10-08 21:00:40 -0700

    step 0

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,12 +1,12 @@
-
-
 import express from 'express'
 import GiftsController from '../controllers/gifts.js'
 
-
 const router = express.Router()
+
 router.get('/', GiftsController.getGifts)
-  
 router.get('/:giftId', GiftsController.getGiftById)
+router.post('/', GiftsController.createGift)
+router.delete('/:id', GiftsController.deleteGift)
+router.patch('/:id', GiftsController.updateGift)
 
 export default router
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import cors from 'cors'
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
 app.use('/gifts', giftsRouter)
